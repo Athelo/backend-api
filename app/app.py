46d21f4eed9cfe692e33b347d0f2002d15a4e54e
Stephanie Syed <stephaniemsyed@utexas.edu>
2023-10-23 13:29:02 -0500

    Remove extraneous files, fix db commit statement

--- app/app.py
+++ app/app.py
@@ -1,17 +1,3 @@
-# Copyright 2022 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from __future__ import annotations
 
 import datetime
@@ -121,7 +107,7 @@ def cast_vote_model() -> Response:
         )
     vote = Vote(candidate=team)
     db.session.add(vote)
-    db.session.commit(vote)
+    db.session.commit()
     return Response(
         status=200,
         response=f"Vote successfully cast for '{vote.candidate}' at time {vote.time_cast}!",
