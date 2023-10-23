0a548fcaa30d7ca2f4a6ff18ccd7efc28a40cfad
Stephanie Syed <stephaniemsyed@utexas.edu>
2023-10-23 13:15:18 -0500

    remove non flask-sqlalchemy code

--- app/app.py
+++ app/app.py
@@ -90,65 +90,6 @@ class Vote(Base):
     candidate: Mapped[str] = mapped_column(nullable=False)
 
 
-def init_connection_pool() -> sqlalchemy.engine.base.Engine:
-    logger.info("init connection pool")
-    """Sets up connection pool for the app."""
-    # use a TCP socket when INSTANCE_HOST (e.g. 127.0.0.1) is defined
-    if os.environ.get("INSTANCE_HOST"):
-        return connect_tcp_socket()
-    logger.info("\tgot host")
-    # use a Unix socket when INSTANCE_UNIX_SOCKET (e.g. /cloudsql/project:region:instance) is defined
-    if os.environ.get("INSTANCE_UNIX_SOCKET"):
-        return connect_unix_socket()
-    logger.info("\tgot unix socket")
-    # use the connector when INSTANCE_CONNECTION_NAME (e.g. project:region:instance) is defined
-    if os.environ.get("INSTANCE_CONNECTION_NAME"):
-        # Either a DB_USER or a DB_IAM_USER should be defined. If both are
-        # defined, DB_IAM_USER takes precedence.
-        logger.info("\tinstance connection provided")
-        return (
-            connect_with_connector_auto_iam_authn()
-            if os.environ.get("DB_IAM_USER")
-            else connect_with_connector()
-        )
-
-    logger.info("\t missing database connection type")
-    raise ValueError(
-        "Missing database connection type. Please define one of INSTANCE_HOST, INSTANCE_UNIX_SOCKET, or INSTANCE_CONNECTION_NAME"
-    )
-
-
-# create 'votes' table in database if it does not already exist
-def migrate_db(db: sqlalchemy.engine.base.Engine) -> None:
-    logger.info("migrate_db")
-    """Creates the `votes` table if it doesn't exist."""
-    with db.connect() as conn:
-        logger.info("\tgot connection")
-        conn.execute(
-            sqlalchemy.text(
-                "CREATE TABLE IF NOT EXISTS votes "
-                "( vote_id SERIAL NOT NULL, time_cast timestamp NOT NULL, "
-                "candidate VARCHAR(6) NOT NULL, PRIMARY KEY (vote_id) );"
-            )
-        )
-        logger.info("\tconnection executed")
-        conn.commit()
-        logger.info("\tcommitted")
-
-
-# This global variable is declared with a value of `None`, instead of calling
-# `init_db()` immediately, to simplify testing. In general, it
-# is safe to initialize your database connection pool when your script starts
-# -- there is no need to wait for the first request.
-
-
-# init_db lazily instantiates a database connection pool. Users of Cloud Run or
-# App Engine may wish to skip this lazy instantiation and connect as soon
-# as the function is loaded. This is primarily to help testing.
-
-old_db = init_connection_pool()
-
-
 @app.route("/votes_model", methods=["GET"])
 def render_index_model() -> str:
     """Serves the index page of the app."""
@@ -189,20 +130,6 @@ def cast_vote_model() -> Response:
         status=200,
         response=f"Vote successfully cast for '{vote.candidate}' at time {vote.time_cast}!",
     )
-
-
-@app.route("/votes", methods=["GET"])
-def render_index() -> str:
-    """Serves the index page of the app."""
-    context = get_index_context(old_db)
-    return render_template("index.html", **context)
-
-
-@app.route("/votes", methods=["POST"])
-def cast_vote() -> Response:
-    """Processes a single vote from user."""
-    team = request.form["team"]
-    return save_vote(old_db, team)
 
 
 @app.route("/", methods=["GET"])
@@ -250,55 +177,6 @@ def get_index_context(db: sqlalchemy.engine.base.Engine) -> dict:
         "recent_votes": votes,
         "tab_count": tab_count,
     }
-
-
-# save_vote saves a vote to the database that was retrieved from form data
-def save_vote(db: sqlalchemy.engine.base.Engine, team: str) -> Response:
-    """Saves a single vote into the database.
-
-    Args:
-        db: Connection to the database.
-        team: The identifier of a team the vote is cast on.
-    Returns:
-        A HTTP response that can be sent to the client.
-    """
-    time_cast = datetime.utcnow()
-    # Verify that the team is one of the allowed options
-    if team != "TABS" and team != "SPACES":
-        logger.warning(f"Received invalid 'team' property: '{team}'")
-        return Response(
-            response="Invalid team specified. Should be one of 'TABS' or 'SPACES'",
-            status=400,
-        )
-
-    # [START cloud_sql_postgres_sqlalchemy_connection]
-    # Preparing a statement before hand can help protect against injections.
-    stmt = sqlalchemy.text(
-        "INSERT INTO votes (time_cast, candidate) VALUES (:time_cast, :candidate)"
-    )
-    try:
-        # Using a with statement ensures that the connection is always released
-        # back into the pool at the end of statement (even if an error occurs)
-        with db.connect() as conn:
-            conn.execute(stmt, parameters={"time_cast": time_cast, "candidate": team})
-            conn.commit()
-    except Exception as e:
-        # If something goes wrong, handle the error in this section. This might
-        # involve retrying or adjusting parameters depending on the situation.
-        # [START_EXCLUDE]
-        logger.exception(e)
-        return Response(
-            status=500,
-            response="Unable to successfully cast vote! Please check the "
-            "application logs for more details.",
-        )
-        # [END_EXCLUDE]
-    # [END cloud_sql_postgres_sqlalchemy_connection]
-
-    return Response(
-        status=200,
-        response=f"Vote successfully cast for '{team}' at time {time_cast}!",
-    )
 
 
 if __name__ == "__main__":
