7d48468816a3ead4a24d81cb6d093716b2e1cee8
Stephanie Syed <stephaniemsyed@utexas.edu>
2023-10-24 12:12:17 -0500

    debug print config

--- athelo/app.py
+++ athelo/app.py
@@ -21,12 +21,12 @@ def set_config(app: Flask):
     match environment.lower():
         case "local":
             config_module = f"{config_module}LocalConfig"
-            print(app.config_module)
+            print(config_module)
         case "production":
             config_module = f"{config_module}ProductionConfig"
         case "staging" | _:
             config_module = f"{config_module}StagingConfig"
-            print(config_module.config)
+            print(config_module)
 
     app.config.from_object(config_module)
     print(app.config)
