77caa37726ef30a1b6c2b4bde4fd283accc080fe
Stephanie Syed <stephaniemsyed@utexas.edu>
2023-10-24 00:43:01 -0500

    Implement User Symptoms, add postman collections

--- athelo/api/__init__.py
+++ athelo/api/__init__.py
@@ -4,5 +4,12 @@ from api.main import main_endpoints
 from api.vote import vote_endpoints
 from api.symptom import symptom_endpoints
 from api.user_profile import user_profile_endpoints
+from api.user_symptom import user_symptom_endpoints
 
-blueprints = [main_endpoints, vote_endpoints, symptom_endpoints, user_profile_endpoints]
+blueprints = [
+    main_endpoints,
+    vote_endpoints,
+    symptom_endpoints,
+    user_profile_endpoints,
+    user_symptom_endpoints,
+]
--- athelo/schemas/user_symptom.py
+++ athelo/schemas/user_symptom.py
@@ -1,11 +1,20 @@
 from models.user_symptom import UserSymptom
 from marshmallow_sqlalchemy import auto_field, SQLAlchemyAutoSchema
+from marshmallow import Schema
 
 
 class UserSymptomSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = UserSymptom
+        include_fk = True
 
     created_at = auto_field(dump_only=True)
     updated_at = auto_field(dump_only=True)
     id = auto_field(dump_only=True)
+
+
+class UserSymptomUpdateSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = UserSymptom
+        include_fk = True
+        exclude = ("id", "created_at", "updated_at", "user_profile_id")
