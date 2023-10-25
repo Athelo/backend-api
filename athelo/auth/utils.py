ffd66fd6cd02dba7c2b67608a26f65f097ca9f3b
Stephanie Syed <stephaniemsyed@utexas.edu>
2023-10-24 23:40:01 -0500

    add error handling for get_user_from_request

--- athelo/auth/utils.py
+++ athelo/auth/utils.py
@@ -1,13 +1,18 @@
 from flask import Request
 from models.database import db
 from models.user_profile import UserProfile
-from werkzeug.exceptions import Unauthorized
+from werkzeug.exceptions import Unauthorized, NotFound
+from sqlalchemy.exc import NoResultFound
 
 
 def get_user_from_request(request: Request):
-    return db.session.execute(
-        db.select(UserProfile).filter_by(email=request.email)
-    ).scalar_one()
+    try:
+        user = db.session.execute(
+            db.select(UserProfile).filter_by(email=request.email)
+        ).scalar_one()
+        return user
+    except NoResultFound:
+        raise NotFound()
 
 
 def is_current_user_or_403(request, user_id):
