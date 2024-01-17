e4b97572cfe1e3c718348edc2d781e0ec8f1f2a9
Lmraza98 <lmraza98@gmail.com>
2024-01-17 15:22:31 -0500

    created PatientProfiles, ProviderProfiles, CaregiveProfiles, AdminProfiles models

--- rest-api/models/admin_profiles.py
+++ rest-api/models/admin_profiles.py
@@ -6,12 +6,11 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.types import Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-class ProviderProfiles(TimestampMixin, Base):
-    __tablename__ = "provider_profiles"
+class AdminProfiles(TimestampMixin, Base):
+    __tablename__ = "admin_profiles"
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     user: Mapped["Users"] = relationship(
-        back_populates="provider_profiles", lazy="joined"
+        back_populates="admin_profiles", lazy="joined"
     )
     active: Mapped[bool] = mapped_column(default=True)
-    appointment_buffer_sec: Mapped[int] = mapped_column(Integer)
