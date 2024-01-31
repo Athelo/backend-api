6d5639a1e3ec4924092b74d4a9543852101f41af
stephaniemsyed <98043158+stephaniemsyed@users.noreply.github.com>
2024-01-31 09:52:02 -0600

    use fake paginiation on symptoms endpoint (#45)

--- rest-api/api/symptom.py
+++ rest-api/api/symptom.py
@@ -1,7 +1,7 @@
 import logging
 from http.client import BAD_REQUEST, CREATED, UNPROCESSABLE_ENTITY
 
-from api.utils import class_route, commit_entity_or_abort
+from api.utils import class_route, commit_entity_or_abort, generate_paginated_dict
 from auth.middleware import jwt_authenticated
 from flask import Blueprint, request
 from flask.views import MethodView
@@ -22,7 +22,7 @@ class SyptomsView(MethodView):
     def get(self):
         symptoms = db.session.scalars(db.select(Symptom)).all()
         schema = SymptomSchema(many=True)
-        return schema.dump(symptoms)
+        return generate_paginated_dict(schema.dump(symptoms))
 
     @jwt_authenticated
     def post(self):
