e74d2d391814f86ce89b263e03897b5f3273f887
Stephanie Syed <stephaniemsyed@utexas.edu>
2024-01-31 22:11:13 -0600

    try a different vonage example

--- rest-api/static/appointment.js
+++ rest-api/static/appointment.js
@@ -164,64 +164,38 @@ async function createTokenForSession() {
         return
     }
 
-
     // Initialize an OpenTok Session object
     var session = OT.initSession(apiKey, selected_session);
 
-
-    // Initialize a Publisher, and place it into the element with id="publisher"
-    var publisher = OT.initPublisher('publisher');
-
-    session.on('streamCreated', function (event) {
-        session.subscribe(event.stream, 'subscriber', {
+    // Subscribe to a newly created stream
+    session.on('streamCreated', (event) => {
+        const subscriberOptions = {
             insertMode: 'append',
             width: '100%',
             height: '100%'
-        }, handleError);
+        };
+        session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
     });
 
-    var publisher = OT.initPublisher('publisher', {
+    session.on('sessionDisconnected', (event) => {
+        console.log('You were disconnected from the session.', event.reason);
+    });
+
+    // initialize the publisher
+    const publisherOptions = {
         insertMode: 'append',
         width: '100%',
         height: '100%'
-    }, handleError);
+    };
+    const publisher = OT.initPublisher('publisher', publisherOptions, handleError);
 
-
-    session.on({
-
-        // This function runs when session.connect() asynchronously completes
-        sessionConnected: function (event) {
-            // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
-            // clients)
-            session.publish(publisher, function (error) {
-                if (error) {
-                    console.error('Failed to publish', error);
-                }
-            });
-        },
-
-        // This function runs when another client publishes a stream (eg. session.publish())
-        streamCreated: function (event) {
-            // Create a container for a new Subscriber, assign it an id using the streamId, put it inside
-            // the element with id="subscribers"
-            var subContainer = document.createElement('div');
-            subContainer.id = 'stream-' + event.stream.streamId;
-            document.getElementById('videoPanel').appendChild(subContainer);
-
-            // Subscribe to the stream that caused this event, put it inside the container we just made
-            session.subscribe(event.stream, subContainer, function (error) {
-                if (error) {
-                    console.error('Failed to subscribe', error);
-                }
-            });
-        }
-
-    });
-
-    // Connect to the Session using the 'apiKey' of the application and a 'token' for permission
-    session.connect(ot_token, function (error) {
+    // Connect to the session
+    session.connect(ot_token, (error) => {
         if (error) {
-            console.error('Failed to connect', error);
+            handleError(error);
+        } else {
+            // If the connection is successful, publish the publisher to the session
+            session.publish(publisher, handleError);
         }
     });
 }
