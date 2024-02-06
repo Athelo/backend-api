3ab095570178e09477e4a1ed35d72d0cbbebe929
Stephanie Syed <stephaniemsyed@utexas.edu>
2024-02-06 09:27:46 -0600

    upgrade gunicorn again

--- rest-api/start.sh
+++ rest-api/start.sh
@@ -10,5 +10,4 @@ flask db upgrade
 # webserver, with one worker process and 8 threads.
 # For environments with multiple CPU cores, increase the number of workers
 # to be equal to the cores available.
-# gunicorn --bind :$PORT --worker-class eventlet --workers 8 --timeout 0 app:app
 gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 app:app
