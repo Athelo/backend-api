38a10443e2102160a25a77f022b64fb20f305445
Stephanie Syed <stephaniemsyed@utexas.edu>
2024-02-10 00:18:37 -0600

    add user password auth to admin app

---
+++ rest-api/static/firebase_auth.js
@@ -0,0 +1,76 @@
+firebase.initializeApp(config);
+
+let token = null;
+let curr_user = null;
+function initApp() {
+    firebase.auth().onAuthStateChanged(async (user) => {
+        if (user) {
+            document.getElementById("message").innerHTML = "Welcome, " + user.email;
+            document.getElementById('googleSignInButton').innerText = 'Sign Out';
+            document.getElementById('passwordAuthContainer').hidden = true
+            curr_user = user
+        }
+        else {
+            document.getElementById("message").innerHTML = "";
+            document.getElementById('googleSignInButton').innerText = 'Sign In with Google';
+            document.getElementById('passwordAuthContainer').hidden = false
+            google_token = ''
+        }
+    });
+}
+window.onload = function () {
+    initApp();
+};
+
+function signIn() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/userinfo.email');
+    firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(result => {
+            // Returns the signed in user along with the provider's credential
+            console.log(`${result.user.displayName} logged in.`);
+            window.alert(`Welcome ${result.user.displayName}!`);
+            window.location = "/dev"
+        })
+        .catch(err => {
+            console.log(`Error during sign in: ${err.message}`);
+            window.alert(`Sign in failed. Retry or check your browser logs.`);
+        });
+}
+
+function signOut() {
+    firebase
+        .auth()
+        .signOut()
+        .catch(err => {
+            console.log(`Error during sign out: ${err.message}`);
+            window.alert(`Sign out failed. Retry or check your browser logs.`);
+        });
+}
+
+// Toggle Sign in/out button
+function toggle() {
+    if (!firebase.auth().currentUser) {
+        signIn();
+    } else {
+        signOut();
+    }
+}
+
+function passwordSignIn() {
+    email = document.getElementById('email').value
+    password = document.getElementById('pass').value
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((result) => {
+            console.log(`${result.user.displayName} logged in.`);
+            window.alert(`Welcome ${result.user.displayName}!`);
+            window.location = "/dev"
+        })
+        .catch((error) => {
+            console.log(`Error during sign in: ${err.message}`);
+            window.alert(`Sign in failed. Retry or check your browser logs.`);
+        });
+
+}
