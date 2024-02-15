76b07bc73d3bdf5d05bbccb227b9fa38e2dd8f8e
stephaniemsyed <98043158+stephaniemsyed@users.noreply.github.com>
2024-02-15 12:42:50 -0600

    bug: user symptom delete and symptom post (#89)
    * fix bug
    
    * fix more bugs

--- rest-api/api/user_symptom.py
+++ rest-api/api/user_symptom.py
@@ -118,22 +118,20 @@ class UserSymptomDetailView(MethodView):
         result = schema.dump(symptom)
         return result, ACCEPTED
 
-    def delete(self, user_profile_id, symptom_id):
-        schema = PatientSymptomSchema()
+    @jwt_authenticated
+    def delete(self, symptom_id):
         symptom = db.session.get(PatientSymptoms, symptom_id)
         if symptom is None:
-            return {
-                "message": f"User Symptom {user_profile_id} does not exist."
-            }, NOT_FOUND
+            return {"message": f"Cannot access user symptom {symptom_id}"}, NOT_FOUND
+        is_current_user_or_403(request, symptom.user_profile_id)
 
-        if symptom.user_profile_id != user_profile_id:
-            return {
-                "message": f"User symptom {symptom_id} does not belong to User {user_profile_id}"
-            }, UNPROCESSABLE_ENTITY
+        schema = PatientSymptomSchema()
+        json_res = schema.dump(symptom)
 
         db.session.delete(symptom)
         db.session.commit()
-        return schema.dump(symptom), ACCEPTED
+
+        return json_res, ACCEPTED
 
 
 @class_route(user_symptom_endpoints, "/user_symptoms/summary/", "my_symptoms_summary")
